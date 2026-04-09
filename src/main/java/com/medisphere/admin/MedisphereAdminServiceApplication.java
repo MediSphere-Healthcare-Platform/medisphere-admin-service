15b464fa6bbc34a6f941928e5211070644d0d572
AroshXR <aroshanasandeep454@gmail.com>
2026-04-09 08:49:56 +0530

    initial commit

---
+++ src/main/java/com/medisphere/admin/MedisphereAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medisphere.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisphereAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisphereAdminServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medisphere/admin/MedisphereAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medisphere.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisphereAdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
