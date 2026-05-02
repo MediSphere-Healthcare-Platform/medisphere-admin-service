d961aaa4a760d1cd8ccbacc7d2bbe05aa5cae259
DasunD2002 <dasun5967@gmail.com>
2026-05-02 14:35:05 +0530

    Fix some issues

--- src/main/java/com/medisphere/admin/controller/AdminController.java
+++ src/main/java/com/medisphere/admin/controller/AdminController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @CrossOrigin
-@RequestMapping("api/v1/admin")
+@RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
 public class AdminController {
 
